0ecae5ea0ebea271fe75b618348e044a3125832c
Jeff Galbraith <jeff.galbraith@xendra.com>
2019-08-07 09:38:25 -0600

    chore(docs): update demo

---
+++ docs/js/2d20f195.bd15c75f.js
@@ -0,0 +1,1 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["2d20f195"],{b1d4:function(t,e,a){"use strict";a.r(e);var n=function(){var t=this,e=t.$createElement,a=t._self._c||e;return a("div",{staticClass:"q-pa-sm"},[a("q-placeholder",{attrs:{animated:""}})],1)},s=[],l=a("2877"),c={},r=Object(l["a"])(c,n,s,!1,null,null,null);e["default"]=r.exports}}]);
